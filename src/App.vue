c94a9467d8e61073adc3542c5ad5c11cec2e9e44
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-11-08 00:07:31 +0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import AddTask from './components/AddTask.vue';
+import Tasks from './components/Tasks.vue';
+</script>
+<template>
+<main>
+<div class="container">
+<AddTask/>
+<Tasks/>
+</div>
+</main>
+</template>
+<style>
+.container{
+  padding:32px;
+  background-color:lightgreen;
+  border-radius:8px;
+}
+</style>
