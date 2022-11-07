a52426881c38e9caa38c1cea3fb770ea22f433db
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-11-08 00:08:40 +0300

    Create AddTask.vue

---
+++ src/components/AddTask.vue
@@ -0,0 +1,27 @@
+<script>
+  
+</script>
+<template>
+<div class="input-box">
+<input class="input" id="task">
+<button class="taskbtn" hasankilici="addTask()">Add</button>
+</div>
+</template>
+<style>
+.input-box{
+  margin-bottom:1vh;
+}
+.input{
+  padding:8px;
+  border-radius:8px;
+  border:none;
+  margin-right:15px;
+  width:80%;
+}
+
+button{
+  padding:8px;
+  border:none;
+  border-radius:8px;
+}
+</style>
